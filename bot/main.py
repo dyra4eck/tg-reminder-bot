5938c183c58c6ded02db710e753de415403e1cad
Тимофей <timafey.sokolov>
2026-04-03 18:54:24 +0300

    feat: update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,9 @@
 import os
+from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start (update: Update, context: ContextTypes.DEFAULT_TYPE):
