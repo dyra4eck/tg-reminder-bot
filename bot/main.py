39fd0f3007507de422461dfcdb6cdd2db09f7655
Тимофей <timafey.sokolov>
2026-04-04 08:48:20 +0300

    feat: remind update

--- bot/main.py
+++ bot/main.py
@@ -6,22 +6,28 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-async def start (update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start_cmd (update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "Привет! Я бот-напоминалка Вахтанг. \n"
         "Напиши /help чтобы узнать, что я умею."
     )
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def help_cmd (update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "/start - начать\n"
         "/help - список команд\n"
         "/remind - (скоро) добавить напоминание"
     )
 
+async def remind_cmd (update: Update, context:ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Я напомню об этом в \n"
+    )
+
 if __name__ == "__main__":
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("start", start_cmd))
+    app.add_handler(CommandHandler("help", help_cmd))
+    app.add_handler(CommandHandler("remind", remind_cmd))
     print("Бот запущен...")
     app.run_polling()
